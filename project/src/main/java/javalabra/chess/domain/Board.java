0399bd36df66029f519243b74d2f34475860d440
Nikita Frolov <nikita@frolov.fi>
2013-01-29 00:10:29 +0200

    use one dimensional array for board

--- project/src/main/java/javalabra/chess/domain/Board.java
+++ project/src/main/java/javalabra/chess/domain/Board.java
@@ -3,19 +3,19 @@ package javalabra.chess.domain;
 
 public class Board {
 
-	private final Square[][] squares;
+	private final Square[] squares;
 
 	public Board() {
-		squares = new Square[8][8];
+		squares = new Square[64];
 		for (int row = 0; row < 8; ++row) {
 			for (int column = 0; column < 8; ++column) {
-				squares[row][column] = new Square(column, row);
+				squares[row * 8 + column] = new Square(column, row);
 			}
 		}
 	}
 
 	public Square getSquare(int column, int row) {
-		return squares[row][column];
+		return squares[row * 8 + column];
 	}
 
 }
